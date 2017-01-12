72362b5529d1a0cc0768c0f520b20c4c04fd20f3
rlakis <robert@192.168.1.103>
2017-01-12 22:02:34 +0200

    clean http header log and add write $_POST to database

--- bin/pfipn.php
+++ bin/pfipn.php
@@ -2,6 +2,7 @@
 ini_set('log_errors_max_len', 0);
 $sandbox = (get_cfg_var('mourjan.server_id')=='99');
 $logfile = '/var/log/mourjan/payfort.log';
+/*
 if (!function_exists('getallheaders')) 
 { 
     function getallheaders() 
@@ -17,29 +18,36 @@ if (!function_exists('getallheaders'))
        return $headers; 
     } 
 } 
+*/
 
 if (!file_exists($logfile)) 
 {
     $fh = @fopen($logfile, 'w');
     fclose($fh);
 }
+
+/*
 $headers = getallheaders();
 $logMsg = '------------------------------------'.PHP_EOL;
 foreach($headers as $key => $value){
     $logMsg.=$key.'::'.$value.PHP_EOL;
 }
 error_log(sprintf("%s\t%s", date("Y-m-d H:i:s"), $logMsg.json_encode($_POST).PHP_EOL), 3, $logfile);
+*/
 
+error_log(sprintf("%s\t%s", date("Y-m-d H:i:s"), json_encode($_POST).PHP_EOL), 3, $logfile);
 
 include_once get_cfg_var('mourjan.path') . '/config/cfg.php';
 include_once $config['dir']. '/core/model/Db.php';
 include_once $config['dir'].'/core/lib/PayfortIntegration.php';
 
 
-$language = $_REQUEST['language'];
-if(!in_array($language, ['en','ar'])){
+$language = filter_input(INPUT_REQUEST, 'language', FILTER_SANITIZE_STRING);
+if (!in_array($language, ['en','ar']))
+{
     $language = 'en';
 }
+
 $payFort = new PayfortIntegration();
 $payFort->setLanguage($language);
 $payment = $payFort->processResponse();
@@ -47,37 +55,54 @@ $payment = $payFort->processResponse();
 
 $success = true;
 $internalError = false;
-if(isset($payment['error_msg'])){
+if(isset($payment['error_msg']))
+{
     $success = false;
 }
 
 $orderId = 0;
 $userId = 0;
-if(isset($payment['merchant_reference'])){
+
+if (isset($payment['merchant_reference']))
+{
     $orderId = preg_split('/-/', $payment['merchant_reference']);
-    if($orderId && count($orderId)==2 && is_numeric($orderId[0]) && is_numeric($orderId[1])){
-        
+    if($orderId && count($orderId)==2 && is_numeric($orderId[0]) && is_numeric($orderId[1]))
+    {
         $userId = (int)$orderId[0];
-        $orderId = (int)$orderId[1];
-        
-    }else{
+        $orderId = (int)$orderId[1];        
+    }
+    else
+    {
         $orderId=0;
     }
 }
-if($orderId){
-    $db = new DB($config);
-    if($success){
+
+$db = new DB($config);
+
+if($orderId)
+{
+    if($success)
+    {
         $res = $db->queryResultArray(
-                    "update t_order set state = ?, msg = ? where id = ? and uid = ? and state = 0 returning id",
+                    "update t_order set state=?, msg=? where id=? and uid=? and state=0 returning id",
                     [2, $payment['fort_id'], $orderId, $userId], TRUE);
-    }else{
+    }
+    else
+    {
         $state = 3;
-        if( ($error_code = substr($payment['response_code'],-3))=="072"){
+        if( ($error_code = substr($payment['response_code'],-3))=="072")
+        {
             $state = 1;
         }
+
         $res = $db->queryResultArray(
-                    "update t_order set state = ?, msg = ? where id = ? and uid = ? and state = 0 returning id",
+                    "update t_order set state=?, msg=? where id=? and uid=? and state=0 returning id",
                     [$state, $payment['error_msg'], $orderId, $userId], TRUE);
     }
 }
+
+$db->queryResultArray("INSERT INTO T_PAYFORT (DATA) VALUES (?)", [json_encode($_POST)], TRUE);
+
+
+
 ?>
