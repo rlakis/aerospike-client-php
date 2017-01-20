4940cc6d2661c0d7a10c2839e68f0bd2e598d1cd
Bassel <bassel@mourjan.com>
2017-01-20 20:36:05 +0200

    bug fix

--- core/lib/MourjanNexmo.php
+++ core/lib/MourjanNexmo.php
@@ -1,7 +1,7 @@
 <?php
 require_once 'vendor/autoload.php';
-require_once get_cfg_var('mourjan.path'). '/config/cfg.php';
-include_once $config['dir'].'/core/lib/nexmo/NexmoMessage.php';
+$dir = get_cfg_var('mourjan.path');
+include_once $dir.'/core/lib/nexmo/NexmoMessage.php';
 
 class MourjanNexmo extends NexmoMessage{
         
