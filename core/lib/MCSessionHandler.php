cc0f5ef35015277270cefd103db0c407bef4c64b
rlakis <robertallakis@192.168.1.100>
2017-03-10 19:58:23 +0200

    aerospike integration

--- core/lib/MCSessionHandler.php
+++ core/lib/MCSessionHandler.php
@@ -1,6 +1,9 @@
 <?php
-ini_set('session.save_handler', 'aerospike');
-ini_set('session.save_path', 'users|sessions|148.251.184.77:3000,138.201.28.229:3000');
+if (ini_get('session.save_handler')!='aerospike')
+{
+    ini_set('session.save_handler', 'aerospike');
+    ini_set('session.save_path', 'users|sessions|148.251.184.77:3000,138.201.28.229:3000');
+}
 
 if (session_status() == PHP_SESSION_NONE) 
 {
