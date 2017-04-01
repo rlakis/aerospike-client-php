1be5f5b7827733f740e17d5b4a9750cb43cd72a6
Bassel <bassel@mourjan.com>
2017-04-01 09:16:46 +0300

    ad size optimization

--- core/layout/Search.php
+++ core/layout/Search.php
@@ -341,28 +341,39 @@ class Search extends Page
             //if ($this->isMobileAd) {
             //$this->set_ad(array('Leaderboard'=>array('/1006833/mourjan-mobile', 320, 50, 'div-gpt-ad-1326381096859-0-'.$this->urlRouter->cfg['server_id'])));
             
-            $this->inlineCss.='.w300,.w650{width:100%;display:block}.w650, .w650 div{margin:0 auto;display: block}.ad100{display:inline-block;width:320px;height:100px}.w650, .w650 div{width:650px;height:120px}.card{overflow-x:hidden}.lbad{text-align:center;overflow:visible!important;background-color:transparent!important;border:0!important}.ad.ad_dt{margin:0}@media all and (max-width: 649px){.w650 {display: none!important}}';
-
+            //$this->inlineCss.='.w300,.w650{width:100%;display:block}.w650, .w650 div{margin:0 auto;display: block}.w650, .w650 div{width:650px;height:120px}.card{overflow-x:hidden}.ad.ad_dt{margin:0}@media all and (max-width: 649px){.w650 {display: none!important}}';
+            $this->inlineCss.='.ad > div{display:inline-block}.lbad{text-align:center;overflow:visible!important;background-color:transparent!important;border:0!important}';
             if (isset($this->user->params['screen'][0]) && $this->user->params['screen'][0]) {
                 $width = $this->user->params['screen'][0];
-                if ($width >= 728) {
+                if ($width >= 745) {
+                    $this->inlineCss .= '.w336 > div{width:336px;height:280px}';
+                    $this->inlineCss .= '.w728 > div{width:728px;height:90px}';
+                    $this->inlineCss .= '@media all and (max-width:744px){.w728{display: none}}';
+                    $this->inlineCss .= '@media all and (max-width:352px){.w336{display: none}}';
                     $this->set_ad(array('Leaderboard' => array('/1006833/M728x90', 728, 90, 'lad-' . $this->urlRouter->cfg['server_id'])));
                     $this->set_ad(array('Square' => array('/1006833/M336x280', 336, 280, 'ad-' . $this->urlRouter->cfg['server_id'])));
                 //} elseif ($width >= 650) {
                 //    $this->set_ad(array('Leaderboard' => array('/1006833/M650x120', 650, 120, 'lad-' . $this->urlRouter->cfg['server_id'])));
                 //    $this->set_ad(array('Square' => array('/1006833/M336x280', 336, 280, 'ad-' . $this->urlRouter->cfg['server_id'])));
-                } elseif ($width >= 468) {
+                } elseif ($width >= 485) {
+                    $this->inlineCss .= '.w336 > div{width:336px;height:280px}';
+                    $this->inlineCss .= '.w468 > div{width:468px;height:60px}';
+                    $this->inlineCss .= '@media all and (max-width:484px){.w468{display: none}}';
+                    $this->inlineCss .= '@media all and (max-width:352px){.w336{display: none}}';
                     $this->set_ad(array('Leaderboard' => array('/1006833/M468x60', 468, 60, 'lad-' . $this->urlRouter->cfg['server_id'])));
                     $this->set_ad(array('Square' => array('/1006833/M336x280', 336, 280, 'ad-' . $this->urlRouter->cfg['server_id'])));
-                } elseif ($width >= 443) {
-                    $this->set_ad(array('Square' => array('/1006833/M336x280', 336, 280, 'ad-' . $this->urlRouter->cfg['server_id'])));
-                    //$this->set_ad(array('Leaderboard' => array('/1006833/M300x100', 300, 100, 'lad-' . $this->urlRouter->cfg['server_id'])));
-                    $this->set_ad(array('Leaderboard' => array('/1006833/M320x50', 320, 50, 'lad-' . $this->urlRouter->cfg['server_id'])));
-                } elseif ($width >= 336) {
+                } elseif ($width >= 353) {
+                    $this->inlineCss .= '.w336 > div{width:336px;height:280px}';
+                    $this->inlineCss .= '.w320 > div{width:320px;height:50px}';
+                    $this->inlineCss .= '.ad100 > div{width:320px;height:100px}';
+                    $this->inlineCss .= '@media all and (max-width:352px){.w336{display: none}}';
                     $this->set_ad(array('Square' => array('/1006833/M336x280', 336, 280, 'ad-' . $this->urlRouter->cfg['server_id'])));
                     //$this->set_ad(array('Leaderboard' => array('/1006833/M300x100', 300, 100, 'lad-' . $this->urlRouter->cfg['server_id'])));
                     $this->set_ad(array('Leaderboard' => array('/1006833/M320x50', 320, 50, 'lad-' . $this->urlRouter->cfg['server_id'])));
                 } else {
+                    $this->inlineCss .= '.w300 > div{width:300px;height:250px}';
+                    $this->inlineCss .= '.w320 > div{width:320px;height:50px}';
+                    $this->inlineCss .= '.ad100 > div{width:320px;height:100px}';
                     $this->set_ad(array('Square' => array('/1006833/M300x250', 300, 250, 'ad-' . $this->urlRouter->cfg['server_id'])));
                     //$this->set_ad(array('Leaderboard' => array('/1006833/M300x100', 300, 100, 'lad-' . $this->urlRouter->cfg['server_id'])));
                     $this->set_ad(array('Leaderboard' => array('/1006833/M320x50', 320, 50, 'lad-' . $this->urlRouter->cfg['server_id'])));
@@ -373,6 +384,9 @@ class Search extends Page
                   $this->set_ad(array('Leaderboard'=>array('/1006833/M468x60', 468, 60, 'mobile-lad-'.$this->urlRouter->cfg['server_id'])));
                   } */
             } else {
+                $this->inlineCss .= '.w300 > div{width:300px;height:250px}';
+                $this->inlineCss .= '.w320 > div{width:320px;height:50px}';
+                $this->inlineCss .= '.ad100 > div{width:320px;height:100px}';
                 $this->set_ad(array('Square' => array('/1006833/M300x250', 300, 250, 'ad-' . $this->urlRouter->cfg['server_id'])));
                 $this->set_ad(array('Leaderboard' => array('/1006833/M320x50', 320, 50, 'lad-' . $this->urlRouter->cfg['server_id'])));
             }
