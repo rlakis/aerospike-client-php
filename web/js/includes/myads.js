0030489afcddb1e0218dea479797eb6bd9dc9b55
rlakis <robertallakis@192.168.1.121>
2019-02-12 19:56:58 +0200

    code optimization

--- web/js/includes/myads.js
+++ web/js/includes/myads.js
@@ -4,17 +4,14 @@ var $=document;
 var articleId=0;
 var articles = document.querySelectorAll("article");
 var len=articles.length;
-for (var x=0; x<len; x++) {
+for(var x=0;x<len;x++){
     articles[x].addEventListener("click", function(e){          
-        //console.log(this.classList.contains('locked'));
-        
         if(e.target.tagName==='A'&&e.target.className===''&&e.target.parentElement.className==='mask'){            
             console.log('article '+e.target.parentElement.nodeName+' > '+e.target.parentElement.className);
             e.stopPropagation();
             return;
         }
         if((e.target.tagName==='DIV'&&e.target.className==='mask')||this.classList.contains('locked')){
-            console.log("sdgkjfskdjhfkjsdhfkjshfjkd");
             e.preventDefault();
             e.stopPropagation();
             return false;
@@ -52,68 +49,46 @@ function getArticle(kID){
     return $.getElementById(kID);
 }
 
-
-function Ad(kID){
-    this.node=$.getElementById(kID);
-    this.headerNode=null;
-    this.editorNode=null;
-}
-Ad.prototype = {
-    exists:function(){return(this.node!==null);},
-    header:function(){        
-        if(this.headerNode===null){
-            this.headerNode=this.node.querySelectorAll('header')[0];
-            this.editorNode=this.headerNode.querySelector('.alloc');
+class Ad{    
+    constructor(id){
+        this._node=$.getElementById(id);
+        if(this._node!==null){
+            this._header=this._node.querySelectorAll('header')[0];
+            this._editor=this._header.querySelector('.alloc');
+            this._message=this._header.querySelector('.msg');
+            this.dataset=this._node.dataset;
         }
-        return this.headerNode;
-    },
-    editor:function(){
-        if(this.editorNode===null){this.header();}
-        return this.editorNode;
-    },
-    getName:function(){
-        return this.editor().innerText;
-    },
-    setName: function(value){        
-        if(this.editor().innerText!=value){
-            this.editor().innerHTML=value+'/'+this.editor().innerText;
-        }
-    },
-    replName:function(value){
-        if(this.editor().innerText!=value){
-            this.editor().innerHTML='<b>'+value+'</b>';
-        }
-    },
-    setSelected:function(){this.node.classList.add('selected');},
-    setLocked:function(){
-        this.node.classList.add('locked');
-        this.addMask();
-        
-    },
-    setUnLocked:function(){
-        this.node.classList.remove('locked');
-        this.removeMask();
-    },
-    addMask:function(){
-        var mask=this.node.querySelector('.mask');
+    }    
+    exists(){return this._node!==null;}
+    dataset(){return this._node.dataset;}
+    header(){return this._header;}    
+    getName(){return this._editor.innerText;}
+    setName(v){if(!this._editor.innerText.includes(v)){this._editor.innerHTML=v+'/'+this._editor.innerText;}}
+    replName(v){this._editor.innerHTML=v;}
+    getMessage(){return this._message.innerText;}
+    setMessage(v){this._message.innerHTML=v;}
+    select(){this._node.classList.add('selected');}
+    lock(){this.setAs('locked');this.addMask();}
+    release(){this.unsetAs('locked');this.removeMask();}
+    setAs(c){this._node.classList.add(c);}
+    unsetAs(c){this._node.classList.remove(c);}
+    addMask(){
+        var mask=this._node.querySelector('.mask');
         if (mask==null){
-            mask=document.createElement("div");
-            mask.style.lineHeight=this.node.offsetHeight+'px';
+            mask=$.createElement("div");
+            mask.style.lineHeight=this._node.offsetHeight+'px';
             mask.className='mask';
-            this.node.appendChild(mask);    
+            this._node.appendChild(mask);    
         }
         return mask;
-    },
-    removeMask:function(){
-        var mask=this.node.querySelector('.mask');
-        if (mask){this.node.removeChild(mask);}
-    },
+    }
+    removeMask(){var mask=this._node.querySelector('.mask');if(mask){this._node.removeChild(mask);}}
     
 }
 
-var editors=document.getElementById('editors');
-const options = {transports: ['websocket'], 'force new connection': false};
-const socket = io.connect("ws.mourjan.com:1313", options);
+var editors=$.getElementById('editors');
+const options={transports: ['websocket'], 'force new connection': false};
+const socket=io.connect("ws.mourjan.com:1313", options);
 socket.on('admins',function(data){
     active_admins=data.a;
     if(isNaN(active_admins)){
@@ -145,15 +120,15 @@ socket.on('admins',function(data){
         for(uid in data.b){
             if(data.b[uid]===0){continue;}
             let ad=new Ad(data.b[uid]);
-            if(!ad.exists()){continue;}
-            ad.replName(editorName(uid));            
-            
-            if(uid==document.body.dataset.key){
-                ad.setSelected();
-            }
-            else{
-                console.log(data.b[uid]);  
-                ad.setLocked();
+            if(ad.exists()){
+                ad.replName(editorName(uid));            
+                if(uid==$.body.dataset.key){
+                    ad.select();
+                }
+                else{
+                    console.log(data.b[uid]);  
+                    ad.lock();
+                }
             }
         }
         /*
@@ -176,8 +151,6 @@ socket.on('admins',function(data){
         }*/
     }
 });
-
-
 socket.on("ad_touch",function(data){
     console.log('touched ', data);
     if(data.hasOwnProperty('x')){
@@ -186,46 +159,38 @@ socket.on("ad_touch",function(data){
             let ad=new Ad(data.i);
             if(ad.exists()){
                 ad.setName(editorName(data.x));
-                ad.setLocked();
-            }
+                ad.lock();
+            }                        
         }
         if(data.hasOwnProperty('o') && data.o>0){
             console.log('release touched by', editorName(data.x));
             let ad=new Ad(data.o);
-            if(ad.exists()){
-                //ad.setName(editorName(data.x));
-                ad.setUnLocked();
-            }
+            if(ad.exists()){ad.release();}
         }
-
-    }
-    
+    } 
 });
-
 socket.on("ad_release",function(data){
     console.log('releasing ', data);
     if(data.hasOwnProperty('i') && data.i>0){
         let ad=new Ad(data.i);
-        if(ad.exists()){ad.setUnLocked();}
+        if(ad.exists()){ad.release();}
     }
 });
-
 socket.on("ads",function(data){
-    if(typeof data.c=='undefined'){ return; }
-    data.c=parseInt(data.c);
-    let article=document.getElementById(data.id);
+    if(typeof data.c=='undefined'){return;}
+    data.c=parseInt(data.c);    
     let ar=(document.body.dir==='rtl');
-    if(article){        
-        console.log(data);
-        let header=article.querySelectorAll('header')[0];        
-        article.dataset.allocatedTo=header.lastChild.innerText;
+    let ad=new Ad(data.id);
+    if(ad.exists()){
+        console.log(data);             
+        //article.dataset.allocatedTo=header.lastChild.innerText;
         var t;
-        var hmsg=header.querySelectorAll('.msg')[0];
-        if (article.dataset.status>=0||c.data==-1){
+        if (ad.dataset.status>=0||c.data==-1){
             switch(data.c){
                 case -1:
                 case 6:
                     t=ar?'تم الحذف':'Deleted';
+                    ad.setMessage(t);
                     break;
                 case 0:
                     t=ar?'جاري التعديل، يجب تحديث الصفحة':'editting in progress, refresh page';
@@ -235,24 +200,24 @@ socket.on("ads",function(data){
                     break;
                 case 2:
                     t=ar?'تمت الموافقة وبإنتظار العرض من قبل محرك مرجان':'Approved and pending Mourjan system processing';
-                    hmsg.textContent=t;
-                    article.classList.add('approved');
+                    ad.setMessage(t);
+                    ad.setAs('approved');
                     break;
                 case 3:              
                     t=ar?'تم رفض عرض هذا الإعلان':'Rejected By Admin';
                     if(typeof data.m!=='undefined'){t+=': '+data.m;}
-                    hmsg.innerHTML=t;
-                    article.classList.add('rejected');
+                    ad.setMessage(t);                                       
+                    ad.setAs('rejected');
                     break;
                 case 7:
-                    var lockedMask=mask(article,1);
+                    var lockedMask=ad.addMask();
                     var link;
-                    if(document.body.dataset.level==9){
-                        var lnks=article.querySelectorAll('div.user > a');
-                        if(lnks && lnks.length>1){link=lnks[1].href+'#'+article.id;}
+                    if($.body.dataset.level==9){
+                        var lnks=ad._node.querySelectorAll('div.user > a');
+                        if(lnks && lnks.length>1){link=lnks[1].href+'#'+ad._node.id;}
                     }
                     else{
-                        link='/myads/'+(ar?'':'en/')+'#'+article.id;
+                        link='/myads/'+(ar?'':'en/')+'#'+ad._node.id;
                     }
                     t=ar?'الإعلان أصبح فعالاً، <a href="'+link+'">انقر(ي) هنا</a> لتفقد الإعلانات الفعالة':'Ad is online now, <a href="'+link+'">click here</a> to view Active Ads';
                     lockedMask.innerHTML=t;
@@ -263,35 +228,22 @@ socket.on("ads",function(data){
         }
     }
 });
-
 socket.on('superAdmin',function(data){
     console.log(data);
     if(typeof data!=='undefined'){
-        //var li=$('#'+data.id);
-        //if(li.length){
-        //    var d=mask(li[0],1);
-        //    d.html('Sent To Super Admin');
-        //}
     }
 });
-
 socket.on('reconnect',function(){
     console.log('Reconnnect to ws');
-});
-      
+});   
 socket.on('connect',function(){
     console.log('connnect to ws');
     if(document.body.dataset.key){this.emit("hook_myads",[document.body.dataset.key,document.body.dataset.level]);}
 });
-
 socket.on('disconnect',function(){
     console.log('disconnect from ws');
-    //li.removeClass('owned used');
-    //wio.emit("hook_myads",[UIDK,ULV]);
 });
 socket.on('event', function(data){console.log('event')});
-
-
 
 
 function editorName(kUID){
@@ -300,17 +252,6 @@ function editorName(kUID){
         return x[0].innerText;
     }
     return 'Anonymous/'+kUID;
-}
-
-
-function mask(e,r){
-    var exists=e.querySelector('.mask');
-    if(exists){e.removeChild(exists);}
-    var d=document.createElement("div");
-    d.style.lineHeight=e.offsetHeight+'px';
-    d.className='mask';
-    e.appendChild(d);    
-    return d;
 }
 
 
