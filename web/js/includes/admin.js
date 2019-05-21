0c17e072a85761fbe2cdff46d25b4d9fa6f57237
rlakis <robertallakis@192.168.1.121>
2019-05-21 19:26:49 +0300

    bug fixes

--- web/js/includes/admin.js
+++ web/js/includes/admin.js
@@ -1,10 +1,16 @@
-
 if(typeof userRaw==='string'){
     var wrapper = document.getElementById("userDIV");
     try {
-        var data = JSON.parse(userRaw);                    
-        var tree = jsonTree.create(data, wrapper);
-        tree.expand();
+        var data = JSON.parse(userRaw); 
+        console.log(data);
+        setTimeout(function(){
+            var tree = jsonTree.create(data, wrapper);
+            tree.expand();
+        }, 100);
+        if(typeof jsonTree==='undefined'){
+            
+        }
+        
     } catch (e) {
         console.log(e);
     }
