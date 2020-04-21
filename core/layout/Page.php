aba7897f8be876ceaac5529f43345ca99b125930
Bassel <bassel@mourjan.com>
2020-04-21 15:52:59 +0300

    new google tag manager implementation

--- core/layout/Page.php
+++ core/layout/Page.php
@@ -3006,12 +3006,16 @@ class Page extends Site {
         ?></script><?php
            
         }
-        
-        echo "\n";
+        ?><!-- Google Tag Manager --><script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-TT7694Q');</script><!-- End Google Tag Manager --><?php
+        //echo "\n";
         if ($this->router->config->get('enabled_ads') && \in_array($this->router->module,['search','detail', 'index'])) {
             ?><script data-ad-client="ca-pub-2427907534283641" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script><?php
         }
-        echo "\n";
+        //echo "\n";
         /*
         //if (!$this->isMobile){
         ?><script type="text/javascript"> //<![CDATA[ 
@@ -4890,10 +4894,14 @@ document.write(unescape("%3Cscript src='https://secure.comodo.com/trustlogo/java
         if ($this->user()->id()) {
             echo ' data-key="', $this->user->info['idKey'], '" data-level=', $this->user()->level()*($this->user()->isSuperUser()?10:1);
         }
-        echo ' data-repo="', $this->router->config->adImgURL, '">', '<meta itemprop="isFamilyFriendly" content="true" />', "\n"; 
+        echo ' data-repo="', $this->router->config->adImgURL, '">';
+        
+        ?><!-- Google Tag Manager (noscript) --><noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TT7694Q" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript><!-- End Google Tag Manager (noscript) --><?php
+        
+        echo '<meta itemprop="isFamilyFriendly" content="true" />', "\n"; 
 
         //error_log('key '.$this->user->info['idKey']. ' decoded ' . $this->user()->decodeId($this->user->info['idKey']) . ' uid '.$this->user()->id());
-        if (1) {  return; }
+        return;
         
         
         
